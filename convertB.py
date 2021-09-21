5391e56a642d60b0a11abcc8eb063e6aada26a14
Johannes-Zi <19johannes99@web.de>
2021-09-21 16:49:47 +0200

    .

--- convertB.py
+++ convertB.py
@@ -8,26 +8,26 @@ import os
 
 def convert_bam(bam_file, fna_file):
     """
-    calculating the per base coverage out of the raw .bam file
+    Calculating the per base coverage out of the raw .bam file
     :param bam_file: file path of the bam file
     :param fna_file: file path of the fna file
     :return: file path of the created coverage file
     """
 
-    # using the path tail for new filename
+    # Using the path tail for new filename
     file_name = os.path.split(bam_file)[1][:-4] + "_coverage.txt"
 
-    # index the assembly file that has been used as a reference
+    # Index the assembly file that has been used as a reference
     command_sam = "samtools faidx " + fna_file
 
-    # compute per base coverage
+    # Compute per base coverage
     command_bed = "bedtools genomecov -ibam " + bam_file + " -d -g my_assembly.fna.fai> " + file_name
 
-    # execute the commands
+    # Execute the commands
     os.system(command_sam)
     os.system(command_bed)
 
-    # return the path of the created coverage .txt file
+    # Return the path of the created coverage .txt file
     return os.path.split(__file__)[0] + "/" + file_name
 
 
