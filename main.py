7d6b67aef56d89f8215a137232b5e7114fe957d4
Johannes-Zi <19johannes99@web.de>
2022-01-12 15:31:56 +0100

    latest Trachinus draco run with up to date CLCR version

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ import outputC
 def main():
     print("MAIN CALLED")
 
-    """"# Short read low cov. region detection:
-    short_read_coverage_file = "/share/gluster/NOTLOESUNG/freya/T_draco/t_draco.pbc.wgs_short.txt"
-    short_low_cov_regions = detectR.detect_regions(short_read_coverage_file, 15, 18)
+    """# Short read low cov. region detection:
+    coverage_file_path = "/share/gluster/NOTLOESUNG/freya/T_draco/t_draco.pbc.wgs_short.txt"
+    low_cov_regions = detectR.detect_regions(coverage_file_path, 15, 18)
     region_count = 0
-    for scaffold in short_low_cov_regions:
+    for scaffold in low_cov_regions:
         region_count += len(scaffold[1])
     print("Total short read low cov. regions before merging: ", region_count)
 
-    low_cov_storage_tsv = "/home/johannes/Desktop/trachinus_draco/TRAdr_healing_run_20.12.21/storage_files/original_low_cov_regions.tsv"
-    detectR.create_low_cov_tsv_file(short_low_cov_regions, low_cov_storage_tsv)
+    low_cov_storage_tsv = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/storage_files/original_low_cov_regions.tsv"
+    detectR.create_low_cov_tsv_file(low_cov_regions, low_cov_storage_tsv)
 
     # Create length distribution plot of the
-    # len_distribution = low_cov_length_distribution_plot(short_low_cov_regions,
+    # len_distribution = low_cov_length_distribution_plot(low_cov_regions,
     #                                                    "/home/johannes/Desktop/low_cov_len_distribution_raw.png")
 
-    short_low_cov_regions = detectR.merge_close_regions(short_low_cov_regions, 250)
+    low_cov_regions = detectR.merge_close_regions(low_cov_regions, 250)
     #"""
 
     """# Create length distribution plot of the
     print("Calculated length distribution")
-    len_distribution = detectR.low_cov_length_distribution_plot(short_low_cov_regions,
+    len_distribution = detectR.low_cov_length_distribution_plot(low_cov_regions,
                                                        "/home/johannes/Desktop/low_cov_len_distribution_merged.png")
     print(len_distribution)
     #"""
 
     """region_count = 0
-    for scaffold in short_low_cov_regions:
+    for scaffold in low_cov_regions:
         region_count += len(scaffold[1])
     print("Total short read low cov. regions after merging: ", region_count)
 
@@ -49,14 +49,14 @@ def main():
                     "t_draco_pacbio_salsa.FINAL_gap_closed.scaff_seqs_FINAL_pilon_2.fasta"
 
     # Create short read low cov. queries:
-    short_read_query_dir = "/home/johannes/Desktop/trachinus_draco/TRAdr_healing_run_20.12.21/short_read_queries/"
-    compD.query_files_creation(short_low_cov_regions, assembly_file, 500, short_read_query_dir, 5000)
+    query_dir_path = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/query_files/"
+    compD.query_files_creation(low_cov_regions, assembly_file, 500, query_dir_path, 5000)
     # """
 
     """# Sending the slurm jobarray to the cluster
     protein_database = "/home/johannes/Desktop/trachinus_draco/protein_db/protein_db.dmnd"
-    input_dir = "/home/johannes/Desktop/trachinus_draco/short_read_queries/"
-    output_dir = "/home/johannes/Desktop/trachinus_draco/short_read_queries_output/"
+    input_dir = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/query_files/"
+    output_dir = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/output_files/"
 
     compD.create_slurmarry(protein_database, input_dir, output_dir)
 
@@ -65,15 +65,19 @@ def main():
 
     # Read in the diamond results
 
-    output_dir = "/home/johannes/Desktop/trachinus_draco/TRAdr_healing_run_20.12.21/short_read_queries_output/"
+    output_dir = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/output_files/"
 
     all_diamond_results = outputP.read_in_diamond_output(output_dir)
 
-    low_cov_storage_tsv = "/home/johannes/Desktop/trachinus_draco/TRAdr_healing_run_20.12.21/storage_files/original_low_cov_regions.tsv"
-    stored_short_low_cov_regions = detectR.read_in_low_cov_tsv_file(low_cov_storage_tsv)
+    low_cov_storage_tsv = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/storage_files/original_low_cov_regions.tsv"
+    stored_low_cov_regions = detectR.read_in_low_cov_tsv_file(low_cov_storage_tsv)
 
     considered_diamond_hits_list, healing_region_list = outputP.filter_out_relevant_results(all_diamond_results, 10,
-                                                                                           stored_short_low_cov_regions)
+                                                                                            stored_low_cov_regions)
+
+    healing_data_path = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/storage_files/healing_data.tsv"
+
+    outputC.create_detailed_healing_information_file(considered_diamond_hits_list, healing_data_path)
 
     #output_path = "/home/johannes/Desktop/considered_hits_len_distribution.png"
     #merged_len_dist = [[5, 71975], [25, 29666], [50, 24082], [100, 32263], [250, 35836], [500, 31516], [1000, 17666],
@@ -102,12 +106,12 @@ def main():
     old_assembly = "/share/gluster/assemblies/Tdraco/" \
                    "t_draco_pacbio_salsa.FINAL_gap_closed.scaff_seqs_FINAL_pilon_2.fasta"
 
-    new_assembly_dir = "/home/johannes/Desktop/trachinus_draco/TRAdr_healing_run_20.12.21/healed_assembly/"
+    new_assembly_dir = "/home/johannes/Desktop/trachinus_draco/healing_runs/TRAdr_healing_run_10.01.2022/healed_assembly/"
 
     new_fna_file_path, simplified_distance_distribution = outputC.heal_assembly_file(healing_region_list, old_assembly,
                                                                                      new_assembly_dir)
 
-    #print(simplified_distance_distribution)
+    print(simplified_distance_distribution)
     
     #"""
 
