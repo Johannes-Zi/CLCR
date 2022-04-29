87a5c668ef9ec0adcfab89223626d7b440f8b3e6
Johannes-Zi <19johannes99@web.de>
2022-04-29 15:58:43 +0200

    added argument parser

--- slurmarry_creation.py
+++ slurmarry_creation.py
@@ -5,6 +5,10 @@ __author__ = "6947325: Johannes Zieres"
 __credits__ = ""
 __email__ = "johannes.zieres@gmail.com"
 
+import os
+import glob
+import argparse
+
 
 def create_slurmarry(protein_database, input_dir, output_dir, slurm_dir):
     """
@@ -44,9 +48,93 @@ def create_slurmarry(protein_database, input_dir, output_dir, slurm_dir):
     return None
 
 
+def prepare_slurm_run(args):
+    """
+    Creates the slurmarray file for the current run, if auto_run ist set True, the job will be automatically submitted
+    to the computer cluster
+    :param args: Arguments from parser
+
+    The following parameters are handed over in the form of args
+    -> project_dir: path to the project directory
+    -> protein_database: path to the protein database
+    -> auto_run: automatic job submitting if True
+
+    :return: None
+    """
+
+    # Initialise args as values for a better overview
+    project_dir = args.project_dir
+    protein_database = args.protein_database
+    auto_run = args.auto_run
+
+    # Delete the slurm directory with all files in it, if it already exists
+    slurm_dir_path = project_dir + "slurm_files/"
+    os_command = " if [ -d " + slurm_dir_path + " ] ; then rm -r " + slurm_dir_path + "; fi"
+    os.system(os_command)
+
+    # Create the new slurm directory
+    os_command = "mkdir " + slurm_dir_path
+    os.system(os_command)
+
+    # Relevant dirs for diamond
+    input_dir = project_dir + "query_files/"
+    output_dir = project_dir + "diamond_output/"
+
+    # Create slurmarray file
+    create_slurmarry(protein_database, input_dir, output_dir, slurm_dir_path)
+
+    # Start slurm job array
+    if auto_run:
+        # Count the input files
+        input_file_list = glob.glob(input_dir + "/temp_in_*.fasta")
+        file_count = len(input_file_list)
+
+        # Submit job
+        os_command = "sbatch --array=1-" + str(file_count) + " " + slurm_dir_path + "CLCR_slurmarray.slurm"
+        os.system(os_command)
+
+    return None
+
+
+def string_to_bool(v):
+    """Enables usage of args True/False"""
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("True", "true"):
+        return True
+    elif v.lower() in ("False", "false"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
-    print("database comparison main executed!")
+    version = "1.0.0"
+    # Initialise parser
+    parser = argparse.ArgumentParser(description="CLCR cluster run",
+                                     epilog="The assembly healing with CLCR consists of 3 different steps:\n"
+                                            "1. Query creation with the create_queries function\n"
+                                            "2. Performing Diamond alignments, when using slurm as scheduler the "
+                                            "prepare_slurm_run function could be used\n"
+                                            "3. Creating a healed assembly version with the create_healed_assembly "
+                                            "function")
 
+    # Differentiate between required and optional arguments
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
 
-if __name__ == '__main__':
-    main()
+    # Required arguments
+    required.add_argument("-p", "--project_dir", action='store', metavar="", type=str, required=True,
+                          help="Path of the project directory")
+    required.add_argument("-c", "--protein_database", action='store', metavar="", type=str, required=True,
+                          help="Path of the protein database")
+
+    # optional arguments
+    optional.add_argument("--auto_run", type=string_to_bool, nargs='?', const=True, default=False,
+                          help="Activate automatic slurm job submission.")
+
+    # Parse args
+    args = parser.parse_args()
+    # Hand over parsed arguments to create_queries function
+    prepare_slurm_run(args)
+
